5ecd644cc5d6840e01f334eadaa94d8c548891ef
Famici <cissezie@yahoo.fr>
2024-02-08 12:01:15 -0800

    1st Modif

--- My_Test_Envir/resources.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_lightsail_instance" "gitlab_test" {
-  name              = "CentOS-1"
-  availability_zone = "us-east-1a"
-  blueprint_id      = "amazon_linux_2"
-  bundle_id         = "nano_1_0"
-  user_data         = "sudo yum install -y httpd && sudo systemctl start httpd && sudo systemctl enable httpd && echo '<h1>  Zie Deployed this Webserver via Terraform on CentOS</h1>' | sudo tee /var/www/html/index.html"
-}
